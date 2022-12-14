5dd76c9328cd3e912260dbc8013199a1012943b8
bubuss <mikolaj.janusz123@gmail.com>
2022-12-14 19:45:34 +0100

    Change bot class to singleton

--- app/bot.py
+++ app/bot.py
@@ -10,6 +10,13 @@ LOGGER = get_logger()
 
 
 class Bot(BotBase):
+    instance = None
+
+    def __new__(cls):
+        if cls.instance is None:
+            cls.instance = super().__new__(cls)
+        return cls.instance
+
     def __init__(self) -> None:
         self.ready = False
         super().__init__(intents=discord.Intents.all())
