0ae1cbee22c4cd30f10fab2c587be2fc773a67bd
bubuss <mikolaj.janusz123@gmail.com>
2023-01-10 20:11:30 +0100

    Add another responder and set default value of next responder to None

--- app/message_responses/responders.py
+++ app/message_responses/responders.py
@@ -2,11 +2,16 @@ from __future__ import annotations
 
 import abc
 import discord
+import logging
 
 from typing import Sequence
 
+logger = logging.getLogger(__name__)
 
 class MessageResponder(abc.ABC):
+    def __init__(self) -> None:
+        self._next_responder: MessageResponder = None
+
     @abc.abstractmethod
     async def get_response(self, message: discord.Message) -> None:
         raise NotImplementedError
@@ -17,8 +22,6 @@ class MessageResponder(abc.ABC):
 
 
 class BaseMessageResponder(MessageResponder):
-    _next_responder: MessageResponder
-
     async def set_next_responder(self, responder: MessageResponder) -> MessageResponder:
         self._next_responder = responder
         return responder
@@ -37,15 +40,24 @@ class PolishBotQuestionResponder(BaseMessageResponder):
         else:
             await super().get_response(message)
 
+class WhoAskedPolishResponder(BaseMessageResponder):
+    async def get_response(self, message: discord.Message) -> None:
+        if "kto pytal" in message.content:
+            await message.channel.send(
+                f"<@{message.author.id}> ja pytalem!"
+            )
+        else:
+            await super().get_response(message)
 
-RESPONDERS: Sequence[MessageResponder] = (PolishBotQuestionResponder(),)
+
+RESPONDERS: Sequence[MessageResponder] = (PolishBotQuestionResponder(), WhoAskedPolishResponder())
 
 
 async def handle_responses(message: discord.Message) -> None:
     first_responder = RESPONDERS[0]
 
     current_responder = first_responder
-    for responder in RESPONDERS:
+    for responder in RESPONDERS[1:]:
         current_responder = await current_responder.set_next_responder(responder)
 
     await first_responder.get_response(message)
